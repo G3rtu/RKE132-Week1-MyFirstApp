5bfe3f3b985245151d7a1c177c7549655c5c9e2a
Gertu Piho <gertu.piho@tktk.ee>
2022-09-03 15:00:14 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment to my code
+
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:");
+//sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
